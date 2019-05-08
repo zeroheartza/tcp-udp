b60683ea13e6c14409fe0576d8c24b4e11832249
zeroheartza <46768647+zeroheartza@users.noreply.github.com>
2019-05-08 23:24:04 +0700

    Add files via upload

---
+++ tcp_sv.py
@@ -0,0 +1,26 @@
+# NOTE - This server can accept only single connection, it can be extended to 
+# support multiple clients/connections using threads.
+import socket
+
+host = '10.6.16.13'        # Symbolic name meaning all available interfaces
+port = 9900     # Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+s.listen(1)
+print("Started server! listeing to {}:{}".format(host, port))
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+
+    try:
+        data = conn.recv(1024)
+        if not data: break
+        print ("Client Says: " + str(data))
+        conn.sendall(str.encode("SERVER_RES: " + str(data)))
+    except socket.error:
+        print ("Error Occured.")
+        break
+
+conn.close()
